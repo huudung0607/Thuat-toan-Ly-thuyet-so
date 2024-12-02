ab2d8d2be6d162374ec95eb5c28d31831db6de0f
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2024-12-02 09:58:44 +0700

    Create UCLN && BCNN.cpp

---
+++ UCLN && BCNN.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+long long gcd(long long a, long long b)
+{
+	if (b == 0)
+	{
+		return a;
+	}
+	return gcd(b, a % b);
+}
+
+long long lcm(long long a, long long b)
+{
+	return a / gcd(a, b) * b;
+}
+
+int main()
+{
+	long long a, b;
+	cin >> a >> b;
+	cout << gcd(a, b) << " " << lcm(a, b) << endl;
+}
