953fd3c6882445239753b6dd6464288437ca6b1d
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-05-25 11:52:23 +0700

    Create Tìm số dư.cpp

---
+++ Tìm số dư.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+#include <climits>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+using namespace std;
+using ll = long long;
+int main()
+{
+	int n, x;
+	cin >> n >> x;
+	ll tmp = 1;
+	for (int i = 1; i <= x; i++) {
+		tmp *= 2;
+		if (tmp > n) {
+			cout << n;
+			return 0;
+		}
+	}
+	cout << n % tmp;
+}
+
